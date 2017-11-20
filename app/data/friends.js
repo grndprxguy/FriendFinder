d934a753c32abbc3c56b5afe5596785b87f1a784
Matthew Nagy <grndprxguy@gmail.com>
2017-11-20 09:44:26 -0500

    initial layout

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+var friends = {
+	"name": "Matt",
+	"photo": "https://vignette.wikia.nocookie.net/muppet/images/0/08/CookieMonsterWaving.jpg/revision/latest?cb=20120128192952",
+	"scores":[
+	  5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+};
+
+module.exports = friends;
